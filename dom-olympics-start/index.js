3dbe6c5cb71ed1d302b900972d8c80f1b3ce6392
Dalton Bunker <dbbunker3@gmail.com>
2021-09-26 10:42:30 -0600

    Fixed clear function

--- dom-olympics-start/index.js
+++ dom-olympics-start/index.js
@@ -23,51 +23,54 @@ var clearBtn = document.getElementById("clear-button");
 clearBtn.addEventListener("click", clearMessages);
 
 function clearMessages() {
-    for (var i = 0; i < messages.length; i++) {
-        messages[i].textContent = "";
+    messagesSize = messages.length
+    for (var i = 0; i < messagesSize; i++) {
+        messages[0].remove();
     }
 }
 
-messages[0].textContent = "Hey What's up";
-messages[1].textContent = "Nothin just chillin with the boyzz";
-messages[2].textContent = "You coming to rage with the south valley later?";
-messages[3].textContent = "Ya dude i'll be there";
+messages[0].textContent = "Hey what are you up to this weekend?";
+messages[1].textContent = "Hitting the slopes with the boyzz";
+messages[2].textContent = "Nice! Hows the snow looking?";
+messages[3].textContent = "looking fresh, better see you up there";
 
 
 // Silver
 var dropDown = document.getElementById("theme-drop-down");
 
-
 dropDown.onchange = updateTheme;
 
 function updateTheme() {
-    console.log("update")
-    var theme = dropDown.value.split('-')
+    var theme = dropDown.value.split('-');
     for (var i = 0; i < messages.length; i++) {
         if (i % 2 === 0) {
-            messages[i].style.backgroundColor = theme[0];
-        } else {
             messages[i].style.backgroundColor = theme[1];
+        } else {
+            messages[i].style.backgroundColor = theme[0];
         }
     }
 }
 
 // Gold
-var sendForm = document["message"]
-var userMessage = sendForm.input.value;
+var sendForm = document.message;
 var sendBtn = sendForm["send-button"];
+
 var messagesContainer = document.querySelector(".messages");
 
 sendBtn.addEventListener("click", addMessage)
 
 function addMessage() {
     newMessage = document.createElement("div");
-    newMessage.textContent = userMessage;
+
+    newMessage.textContent = sendForm.input.value;
     newMessage.classList.add("message");
+    var theme = dropDown.value.split('-');
     if (messages.length % 2 == 0) {
         newMessage.classList.add("left");
+        newMessage.style.backgroundColor = theme[1];
     } else {
         newMessage.classList.add("right");
+        newMessage.style.backgroundColor = theme[0];
     }
     messagesContainer.appendChild(newMessage);
 }
