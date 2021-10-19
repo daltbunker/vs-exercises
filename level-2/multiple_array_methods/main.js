76aa83c4ab407847fbb63a6c5a098093340dae12
Dalton Bunker <dbbunker3@gmail.com>
2021-10-19 13:52:36 -0600

    first completion

---
+++ level-2/multiple_array_methods/main.js
@@ -0,0 +1,57 @@
+function sortedOfAge(arr){
+    arr.sort((a, b) => {
+        let i = 0; 
+        a = a.lastName
+        b = b.lastName
+        while (a.charCodeAt(i) === b.charCodeAt(i)){
+            i++;
+        }
+        return a.charCodeAt(i) - b.charCodeAt(i)
+    })
+    return arr.reduce((result, person) => {
+        if (person.age > 18){
+            result.push(`<li>${person.firstName} ${person.lastName} is ${person.age}</li>`);
+        }
+        return result;
+    }, [])
+ }
+ 
+ var peopleArray = [
+     {
+         firstName: "Sarah",
+         lastName: "Palin",
+         age: 47
+        },
+        {
+            firstName: "Frank",
+            lastName: "Zappa",
+            age: 12
+        },
+        {
+            firstName: "Rick",
+            lastName: "Sanchez",
+            age: 78
+        },
+        {
+            firstName: "Morty",
+            lastName: "Smith",
+            age: 29
+        },
+        {
+            firstName: "Kyle",
+            lastName: "Mooney",
+            age: 27
+        },
+        {
+            firstName: "Pasha",
+            lastName: "Datsyuk",
+            age: 13
+        },
+        {
+            firstName: "Lev",
+            lastName: "Tolstoy",
+            age: 82
+        }
+    ]
+
+console.log(sortedOfAge(peopleArray));
