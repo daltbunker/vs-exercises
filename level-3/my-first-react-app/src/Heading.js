f9537f08f9252a640726c0ecc9eab484776a184f
Dalton Bunker <dbbunker3@gmail.com>
2021-11-16 14:02:38 -0700

    updated header styles

--- level-3/my-first-react-app/src/Heading.js
+++ level-3/my-first-react-app/src/Heading.js
@@ -4,7 +4,7 @@ const Heading = ({ title }) => {
     return (
         <div>
             <h1 style={{ 
-                textAlign: 'center', color: 'rgba(0, 0, 0)', fontSize: 50, fontWeight: 800, padding: 9, 
+                textAlign: 'center', color: '#000000', fontSize: 50, fontWeight: 800, padding: 9, 
                 borderBottom: '3px solid #000000'
                 }}>{title}</h1>
         </div>
