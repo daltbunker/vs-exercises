db8a0c6df8a01dfdcd157436995d13c3af30c66a
Dalton Bunker <dbbunker3@gmail.com>
2021-11-22 16:07:07 -0700

    fixed selected event

--- level-3/dice/src/components/DiceBox.js
+++ level-3/dice/src/components/DiceBox.js
@@ -9,11 +9,11 @@ export default class Dicebox extends Component {
         super()
         this.state = {
             dice: [
-                1,
-                2,
-                3,
-                4,
-                5
+                0,
+                0,
+                0,
+                0,
+                0
             ],
             selectedDice: [
                 false,
@@ -29,31 +29,44 @@ export default class Dicebox extends Component {
     }
 
     rollDice() {
-        const diceArr = []
-        for (let i = 0; i < 5; i++) {
-            diceArr.push(Math.floor(Math.random() * 6 + 1))
-        }
-        this.setState(() => {
+        this.setState(prevState => {
+            if (prevState.rollCount >= 3) {
+                return {
+                    dice: [0, 0, 0, 0, 0],
+                    selectedDice: [false, false, false, false, false],
+                    rollCount: 0
+                }
+            }
+            for (let i = 0; i < 5; i++) {
+                if (!prevState.selectedDice[i]) {
+                    prevState.dice.splice(i, 1, Math.floor(Math.random() * 6 + 1))
+                }
+            }
             return {
-                dice: diceArr
+                dice: prevState.dice,
+                rollCount: prevState.rollCount + 1
             }
         })
     }
 
-    selectedEvent(num) {
-       console.log(num)
+    selectedEvent(index) {
+        this.setState(prevState => {
+            return {
+                selectedDice: prevState.selectedDice.map((selected, i) => i === index ? !selected : selected)
+            }
+        })
     }
 
     render() {
         const diceArr = this.state.dice.map((num, i) => {
-            return <Die num={num} selected={this.state.selectedDice[i]} onClick={this.selectedEvent} key={i}/>
+            return <Die num={num} selected={this.state.selectedDice[i]} clickEvent={this.selectedEvent} index={i} key={i}/>
         })
         return (
         <div className='dice-box'>
             <div className="dice-container">
                 {diceArr}
             </div>
-            <Button text='RESET' bkgColor='red' onClick={this.rollDice} />
+            <Button text={this.state.rollCount >= 3 ? 'RESTART' : 'ROLL'} bkgColor='red' onClick={this.rollDice} />
         </div>
         )
     }
--- level-3/dice/src/components/Die.js
+++ level-3/dice/src/components/Die.js
@@ -1,9 +1,9 @@
 import '../styles/Die.css'
 import React from 'react'
 
-function Die({ num, selected, clickEvent }) {
+function Die({ num, selected, clickEvent, index }) {
     return (
-        <div className='die' onClick={clickEvent} style={{ backgroundColor: selected ? 'rgba(128, 128, 128, 0.462)' : 'none' }}>{ num }</div>
+        <div className='die' onClick={() => clickEvent(index)} style={{ backgroundColor: selected ? 'rgba(128, 128, 128, 0.462)' : 'white' }}>{ num }</div>
     )
 }
 
