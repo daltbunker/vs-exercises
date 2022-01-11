27100e7b9ed4d9ae84783ea3b010c5d944bc673d
Dalton Bunker <dbbunker3@gmail.com>
2022-01-11 14:13:05 -0700

    edited margin on title

--- level-4/plumbing/src/App.js
+++ level-4/plumbing/src/App.js
@@ -2,24 +2,19 @@ import './App.css';
 import {NavLink, Outlet} from 'react-router-dom'
 import Footer from './components/Footer';
 
-const navStyles = {
-  textDecoration: "none",
-  color: "black",
-  margin: 5,
-  textTransform: "uppercase"
-
-
-}
-// style={({ isActive }) => {
-//   return {
-//     color: isActive ? "red" : ""
-//   };
-// }} 
 
 function App() {
+
+  const navStyles = {
+    textDecoration: "none",
+    color: "black",
+    margin: 5,
+    textTransform: "uppercase"
+  }
+
   return (
     <div className="app" style={{minHeight: "100vh", position: "relative"}}>
-      <h1 style={{marginLeft: 15}}>The Plumbing Guys</h1>
+      <h1 style={{marginLeft: 50}}>The Plumbing Guys</h1>
       <nav style={{display: 'flex', justifyContent: 'space-evenly'}}>
         <NavLink to="/home" style={({ isActive }) => {
           return {
@@ -39,7 +34,6 @@ function App() {
           return {
             ...navStyles,
             borderBottom: isActive ? "1px solid black" : "none",
-
           }
         }}>About</NavLink>
       </nav>
