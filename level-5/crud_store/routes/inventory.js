314cbfe821fb799f87848310e6a243e9e8849695
Dalton Bunker <dbbunker3@gmail.com>
2022-02-06 17:25:33 -0700

    crud operations working

--- level-5/crud_store/routes/inventory.js
+++ level-5/crud_store/routes/inventory.js
@@ -2,7 +2,6 @@ const express = require("express")
 const InventoryModel = require("../models/inventory")
 const inventory = express.Router()
 
-// get, post
 inventory.route("/")
     .get((req, res, next) => {
         InventoryModel.find((err, guitars) => {
@@ -24,7 +23,44 @@ inventory.route("/")
         })
     })
 
-// get one, put, delete
 inventory.route("/:inventoryId")
+    .get((req, res, next) => {
+        InventoryModel.find(
+            { _id: req.params.inventoryId },
+            (err, guitar) => {
+                if (err) {
+                    res.status(500)
+                    return next(err)
+                }
+                return res.status(200).send(guitar)
+            }
+        )
+    })
+    .put((req, res, next) => {
+        InventoryModel.findOneAndUpdate(
+            { _id: req.params.inventoryId },
+            req.body,
+            { new: true },
+            (err, updatedGuitar) => {
+                if (err) {
+                    res.status(500)
+                    return next(err)
+                }
+                return res.status(201).send(updatedGuitar)
+            }
+        )
+    })
+    .delete((req, res, next) => {
+        InventoryModel.findByIdAndDelete(
+            { _id: req.params.inventoryId },
+            (err, deletedGuitar) => {
+                if (err) {
+                    res.status(500)
+                    return next(err)
+                }
+                return res.status(200).send(`deleted "${deletedGuitar.brand} ${deletedGuitar.model}" from database`)
+            }
+        )
+    })
 
 module.exports = inventory
